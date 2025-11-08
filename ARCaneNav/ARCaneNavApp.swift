95b55c127ade9ffaf949713393ff87e99b8adcaf
riteshbhirud <riteshbhirud@gmail.com>
2025-11-07 19:28:48 -0500

    Initial Commit

---
+++ ARCaneNav/ARCaneNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARCaneNavApp.swift
+//  ARCaneNav
+//
+//  Created by ritesh on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ARCaneNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARCaneNav/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARCaneNav
+//
+//  Created by ritesh on 11/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
